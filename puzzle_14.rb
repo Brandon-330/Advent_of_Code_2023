bfa737339ee95816390a237092f24a13856f754e
Brandon-330 <brandonsamlima@gmail.com>
2023-12-15 21:43:36 -0500

    puzzle 14 done! :)

--- puzzle_14.rb
+++ puzzle_14.rb
@@ -1,4 +1,96 @@
-def calculate_total(map)
+old_map = File.new('texts/puzzle_14.txt').read.split("\n")
+map = old_map
+
+hsh = Hash.new
+start_of_loop_idx = 0
+end_of_loop_idx = 0
+counter = 0
+
+loop do
+  hsh[map.dup.join] = counter
+
+  # North
+  map.each_with_index do |line, line_idx|
+    if line_idx >= 1
+      line.chars.each_with_index do |char, char_idx|
+        if char == 'O'
+          times_rolled = 0
+          loop do
+            break if ((line_idx - times_rolled) - 1 < 0) || /[#O]/.match?(map[(line_idx - times_rolled) - 1][char_idx])
+            times_rolled += 1
+          end
+
+          map[line_idx][char_idx] = '.'
+          map[line_idx - times_rolled][char_idx] = 'O'
+        end
+      end
+    end
+  end
+
+  # West
+  map.each_with_index do |line, line_idx|
+    line.chars.each_with_index do |char, char_idx|
+      if char_idx >= 1
+        if char == 'O'
+          times_rolled = 0
+          loop do
+            break if ((char_idx - times_rolled) - 1 < 0) || /[#O]/.match?(map[line_idx][(char_idx - times_rolled) - 1])
+            times_rolled += 1
+          end
+
+          map[line_idx][char_idx] = '.'
+          map[line_idx][char_idx - times_rolled] = 'O'
+        end
+      end
+    end
+  end
+  
+  # South
+  map.reverse!.each_with_index do |line, line_idx|
+    if line_idx >= 1
+      line.chars.each_with_index do |char, char_idx|
+        if char == 'O'
+          times_rolled = 0
+          loop do
+            break if ((line_idx - times_rolled) - 1 < 0) || /[#O]/.match?(map[(line_idx - times_rolled) - 1][char_idx])
+            times_rolled += 1
+          end
+
+          map[line_idx][char_idx] = '.'
+          map[line_idx - times_rolled][char_idx] = 'O'
+        end
+      end
+    end
+  end.reverse!
+
+  map.map! do |line|
+    line.reverse
+  end
+
+  # East
+  map.each_with_index do |line, line_idx|
+    line.chars.each_with_index do |char, char_idx|
+      if char_idx >= 1
+        if char == 'O'
+          times_rolled = 0
+          loop do
+            break if ((char_idx - times_rolled) - 1 < 0) || /[#O]/.match?(map[line_idx][(char_idx - times_rolled) - 1])
+            times_rolled += 1
+          end
+
+          map[line_idx][char_idx] = '.'
+          map[line_idx][char_idx - times_rolled] = 'O'
+        end
+      end
+    end
+  end
+
+  map.map! do |line|
+    line.reverse
+  end
+
+  counter += 1
+
   total = 0
   map.each_with_index do |line, line_idx|
     sum_load = (map.size - line_idx)
@@ -7,30 +99,16 @@ def calculate_total(map)
       total += sum_load if char == 'O'
     end
   end
+  puts counter if total == 65
 
-  total
-end
-
-old_map = File.new('texts/puzzle_14.txt').read.split("\n")
-map = old_map
-previous_map = map.clone
-
-memoize = Hash.new
-counter = 0
-start_loop = 0
-
-loop do
-  p memoize
-
-  if memoize.key?(map)
-    puts map if calculate_total(map) == 64
-    old_map = map.dup
-    map = memoize[map]
-    break if counter == 1000000000
-    p counter += 1
-    next
+  if hsh.key?(map.dup.join)
+    start_of_loop_idx = hsh[map.dup.join]
+    break
   end
+end
+new_counter = (1000000000 - start_of_loop_idx) % (counter - start_of_loop_idx)
 
+new_counter.times do |_|
   # North
   map.each_with_index do |line, line_idx|
     if line_idx >= 1
@@ -110,23 +188,11 @@ loop do
   map.map! do |line|
     line.reverse!
   end
-
-  memoize[previous_map] = map
-
-  previous_map = map
-
-  counter += 1
 end
-p counter
-p (counter - start_of_loop_idx)
-
-f = File.new('texts/puzzle_14_results.txt', 'w')
 
 total = 0
 map.each_with_index do |line, line_idx|
   sum_load = (map.size - line_idx)
-
-  f.write(line + "\n")
 
   line.each_char do |char|
     total += sum_load if char == 'O'
