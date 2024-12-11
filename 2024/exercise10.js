a3809e2097c3f659b47148985d01ecdaec38bd7f
Brandon Lima <brandonsamlima@gmail.com>
2024-12-10 20:46:03 -0500

    Wrapped up day 10

--- 2024/exercise10.js
+++ 2024/exercise10.js
@@ -1,3 +1,4 @@
+
 let fs = require('fs');
 
 function formatCoordinateTable(data) {
@@ -41,3 +42,50 @@ fs.readFile('input10.txt', 'utf-8', function(err, data) {
 
   console.log(result);
 });
+
+
+
+// let fs = require('fs');
+
+// function formatCoordinateTable(data) {
+//   let rows = data.split('\r\n');
+//   let table = rows.map(row => row.split(''));
+//   return table;
+// }
+
+// function pathsToPeak(table, i, j, num = 0, memo = {}) {
+//   if (i < 0 || j < 0 || i >= table.length || j >= table[0].length) {
+//     return 0;
+//   } else if (!memo[[i, j]] && Number(table[i][j]) === 9 && num === 9) {
+//     memo[[i, j]] = true;
+//     return 1;
+//   } else if (Number(table[i][j]) === num) {
+//     let newNum = num + 1;
+//     return (
+//       pathsToPeak(table, i - 1, j, newNum, memo) +
+//       pathsToPeak(table, i + 1, j, newNum, memo) +
+//       pathsToPeak(table, i, j - 1, newNum, memo) +
+//       pathsToPeak(table, i, j + 1, newNum, memo)
+//     );
+//   } else {
+//     return 0;
+//   }
+// }
+
+// fs.readFile('input10.txt', 'utf-8', function(err, data) {
+//   let table = formatCoordinateTable(data);
+
+//   let result = 0;
+//   for (let rowIdx = 0; rowIdx < table.length; rowIdx += 1) {
+//     let row = table[rowIdx];
+//     for (let colIdx = 0; colIdx < row.length; colIdx += 1) {
+//       let el = row[colIdx];
+
+//       if (el === '0') {
+//         result += pathsToPeak(table, rowIdx, colIdx);
+//       }
+//     }
+//   }
+
+//   console.log(result);
+// });
